8dc23e932dd2ab3b4528fc0e9d58a6be64166408
Fredrik Welander <welandfr@arcada.fi>
2025-11-19 13:48:24 +0200

    fixes

--- lektion-8/lightbox.js
+++ lektion-8/lightbox.js
@@ -5,24 +5,25 @@ for (const id of IMAGE_ID) {
     `;
 }
 
-for (const img of document.querySelectorAll("#thumbnail-container > img")) {
-    console.log(img);
-    img.addEventListener('click', () => {
-        const id = img.dataset.id; // samma som img.getAttribute("data-id")
-        document.getElementById("lightbox").innerHTML = `
+function openLightbox(id) {
+    document.getElementById("lightbox").innerHTML = `
             <img src="./lightbox-img/img-0${id}.png">
         `;
-        document.getElementById("lightbox").classList.remove("hidden");
-    });
+    document.getElementById("lightbox").classList.remove("hidden");
 }
 function closeLightbox() {
     document.getElementById("lightbox").classList.add("hidden");
 }
 
+for (const img of document.querySelectorAll("#thumbnail-container > img")) {
+    img.addEventListener('click', () => openLightbox(img.dataset.id)); // samma som img.getAttribute("data-id")
+}
+
+// Stäng bilden men klick
 document.querySelector("#lightbox").addEventListener('click', closeLightbox);
-// stäng bilden med Esc
+// Stäng bilden med Esc
 window.addEventListener('keydown', (evt) => {
-    if (!document.getElementById("lightbox").classList.contains("hidden") 
+    if (!document.getElementById("lightbox").classList.contains("hidden")
         && evt.key === "Escape") {
         closeLightbox();
     }
